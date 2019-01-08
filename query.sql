89720be1d7d5e693db4e7075eb41a4203062463d
Plutone11011 <lorenzo97@fastwebnet.it>
2019-01-08 14:48:53 +0100

    insert stronzi

---
+++ query.sql
@@ -0,0 +1,4 @@
+INSERT COMPAGNIA(CodiceCompagnia, Nome, Nazione) VALUES('C1','CompagniaC1','Italia') ;
+
+INSERT DIPENDENTE(CodiceFiscale, Nome, Cognome, Email, Telefono, Nazionalita, DataDiNascita) 
+VALUES('RSIMROJ60M21G','Mario','Rossi',"mario.rossi@gmail.com","3458761213","italiana",1990-03-12) ;
