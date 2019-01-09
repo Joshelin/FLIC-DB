0cdaf906224cbd2169e98e48b3045aa4700e9f95
Mango5 <chiara.mengoli5@studio.unibo.it>
2019-01-09 17:17:43 +0100

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,12 @@
 
+//evitare che lo stesso dipendente possa essere Comandante e Hostess dello stesso equipaggio.
+
+
+//evitare che un dipendente possa essere passeggero di un volo assegnato al suo equipaggio.
+
+
+
+//evitare che l'aeroporto di partenza e destinazione di un volo siano uguali.
+
+
+
